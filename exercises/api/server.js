69238f1c569d73d11a37a408c679edaf1b32f84c
parthshah <parth.shah@sprinklr.com>
2018-12-17 01:30:22 +0530

    3rd exercise solution added

--- exercises/api/server.js
+++ exercises/api/server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const mime = require('mime');
 const url = require('url')
 const fs = require('fs')
 const path = require('path')
@@ -9,30 +10,48 @@ const path = require('path')
  */
 const findAsset = (name) => {
   const assetPath = path.join(__dirname, 'assets', name)
-  return fs.readFileSync(assetPath, {encoding: 'utf-8'}).toString()
+  return new Promise((resolve, reject) => {
+    fs.readFile(assetPath, { encoding: 'utf-8' }, (error, response) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(response);
+      }
+    });
+  });
 }
 
 const hostname = '127.0.0.1'
 const port = 3000
-
-// log incoming request coming into the server. Helpful for debugging and tracking
+const router = {
+    '/ GET': {
+      asset: 'index.html',
+      mime: mime.getType('html'),
+    },
+    '/style.css GET': {
+      asset: 'style.css',
+      mime: mime.getType('css'),
+    }
+  }
+  // log incoming request coming into the server. Helpful for debugging and tracking
 const logRequest = (method, route, status) => console.log(method, route, status)
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
   const method = req.method
   const route = url.parse(req.url).pathname
-  // this is sloppy, espcially with more assets, create a "router"
-  if (route === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(findAsset('index.html'))
-    logRequest(method, route, 200)
+  const matchRoute = router[`${route} ${method}`];
+  if (!matchRoute) {
+    res.writeHead(404);
+    logRequest(method, route, 404)
     res.end()
-  } else {
-    // missing asset should not cause server crash
-    throw new Error('route not found')
-    res.end()
+    return;
   }
-  // most important part, send down the asset
+  // this is sloppy, espcially with more assets, create a "router"
+  res.writeHead(200, { 'Content-Type': matchRoute.mime })
+  res.write(await findAsset(matchRoute.asset))
+  logRequest(method, route, 200)
+  res.end()
+    // most important part, send down the asset
 })
 
 server.listen(port, hostname, () => {
